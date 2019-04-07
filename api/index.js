e921d088f26dd25c1a2cd6c3dc217655414ccbac
simon3000 <simon3000@163.com>
2019-04-07 22:24:52 +0800

    biliAPI test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const biliAPI = require('bili-api')
 const { init } = require('./database')
 
 const vtbs = require('./vtbs')
@@ -5,4 +6,15 @@ const vtbs = require('./vtbs')
 ;
 (async () => {
   let { site, info, active, live } = await init()
+  let vtb = await biliAPI(vtbs[0], ['mid', 'uname', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'online'])
+  console.log('mid', vtb.mid)
+  console.log('uname', vtb.uname)
+  console.log('roomid', vtb.roomid)
+  console.log('sign', vtb.sign)
+  console.log('notice', vtb.notice)
+  console.log('follower', vtb.follower)
+  console.log('archiveView', vtb.archiveView)
+  console.log('guardNum', vtb.guardNum)
+  console.log('liveStatus', vtb.liveStatus)
+  console.log('online', vtb.online)
 })()
