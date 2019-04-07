3f2006dac8e1c58c8785d0467498c84566e96b24
simon3000 <simon3000@163.com>
2019-04-07 20:31:20 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,7 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'space-before-function-paren': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
