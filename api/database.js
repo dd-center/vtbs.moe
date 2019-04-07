746fd66b0f8697b09bf5b6f4f6c1888ce84e5062
simon3000 <simon3000@163.com>
2019-04-07 20:53:45 +0800

    level database

--- api/database.js
+++ api/database.js
@@ -1,5 +1,19 @@
+const level = require('level')
 const fs = require('fs-extra')
+
+class LevelDatabase {
+  constructor({ name }) {
+    this.db = level(`db/${name}`, { valueEncoding: 'json' })
+  }
+  put(key, value) {
+    return this.db.put(key, value)
+  }
+  get(key) {
+    return this.db.get(key).catch(() => undefined)
+  }
+}
 
 exports.init = async () => {
   await fs.ensureDir('./db')
+  let sign = new LevelDatabase({ name: 'sign' })
 }
