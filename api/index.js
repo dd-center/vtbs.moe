b91fb8249b4d555d0fb4afa7ba10252658fd59fe
simon3000 <simon3000@163.com>
2019-04-07 22:59:15 +0800

    parallel

--- api/index.js
+++ api/index.js
@@ -3,18 +3,33 @@ const { init } = require('./database')
 
 const vtbs = require('./vtbs')
 
+const PARALLEL = 3
+const INTERVAL = 1000
+
+class Spider {
+  constructor({ db, spiderId, PARALLEL, INTERVAL }) {
+    this.db = db
+    this.spiderId = spiderId
+    this.PARALLEL = PARALLEL
+    this.INTERVAL = INTERVAL
+  }
+  wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)) }
+  async start() {
+    for (;;) {
+      let startTime = (new Date()).getTime()
+      console.log(`${this.spiderId}/${this.PARALLEL}`)
+      let endTime = (new Date()).getTime()
+      await this.wait(this.INTERVAL - (endTime - startTime))
+    }
+  }
+}
+
 ;
 (async () => {
-  let { site, info, active, live } = await init()
-  let vtb = await biliAPI(vtbs[0], ['mid', 'uname', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'online'])
-  console.log('mid', vtb.mid)
-  console.log('uname', vtb.uname)
-  console.log('roomid', vtb.roomid)
-  console.log('sign', vtb.sign)
-  console.log('notice', vtb.notice)
-  console.log('follower', vtb.follower)
-  console.log('archiveView', vtb.archiveView)
-  console.log('guardNum', vtb.guardNum)
-  console.log('liveStatus', vtb.liveStatus)
-  console.log('online', vtb.online)
+  let db = await init()
+  // let vtb = await biliAPI(vtbs[0], ['mid', 'uname', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'online'])
+  for (const spiderId of Array(PARALLEL).fill().map((current, index) => index)) {
+    let spider = new Spider({ db, spiderId, PARALLEL, INTERVAL })
+    spider.start()
+  }
 })()
