0ea0a3ca57437dcc844db45b5d2185e65e32f5cd
simon3000 <simon3000@163.com>
2019-04-08 23:33:45 +0800

    add api link

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import io from 'socket.io-client'
 
 const socket = io('http://localhost:8001')
+// const socket = io('https://api.vtb.simon3k.moe')
 
 socket.on('connect', () => {
   console.log('connect')
