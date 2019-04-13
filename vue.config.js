310fb88f25b1e607d364531ddfb92613a6380934
simon3000 <simon3000@163.com>
2019-04-14 07:26:43 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon-1024x1024.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg'
+    }
+  }
 }
