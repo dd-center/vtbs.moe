2643c4deed5739215e6a83a29f39145407d5b37e
simon3000 <simon3000@163.com>
2019-04-14 20:38:44 +0800

    load face when info

--- src/store.js
+++ src/store.js
@@ -49,19 +49,17 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    async SOCKET_vtbs({ commit, state }, data) {
-      for (let i = 0; i < data.length; i++) {
-        let mid = data[i].mid
-        if (!state.face[mid]) {
-          let time = (new Date()).getTime()
-          let face = await db.get(`face_${mid}`).catch(() => undefined)
-          if (face && time - face.time < 1000 * 60 * 60 * 3) {
-            commit('loadFace', { mid, face: face.data })
-          } else {
-            face = `data:image/png;base64,${await get('face', mid)}`
-            commit('loadFace', { mid, face })
-            await db.put(`face_${mid}`, { time, data: face })
-          }
+    async SOCKET_info({ commit, state }, data) {
+      let mid = data.mid
+      if (!state.face[mid]) {
+        let time = (new Date()).getTime()
+        let face = await db.get(`face_${mid}`).catch(() => undefined)
+        if (face && time - face.time < 1000 * 60 * 60 * 3) {
+          commit('loadFace', { mid, face: face.data })
+        } else {
+          face = `data:image/png;base64,${await get('face', mid)}`
+          commit('loadFace', { mid, face })
+          await db.put(`face_${mid}`, { time, data: face })
         }
       }
     }
