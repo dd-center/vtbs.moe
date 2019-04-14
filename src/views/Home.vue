f3637536628fd58c27b8a54df9611223a0fceb81
simon3000 <simon3000@163.com>
2019-04-14 08:12:28 +0800

    use followerRank

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
   <el-main>
     <el-row type="flex" justify="space-around">
       <el-col :xs="24" :sm="20" :md="16" :lg="13" :xl="12" v-loading="!vtbs.length">
-        <card v-for="vtb in vtbs" :vtb="vtb" :key="vtb.mid"></card>
+        <card v-for="vtb in followerRank" :vtb="vtb" :key="vtb.mid"></card>
       </el-col>
     </el-row>
   </el-main>
