43da04860a04c920651457a0c89e665f3b0dde9e
simon3000 <simon3000@163.com>
2019-04-14 08:12:21 +0800

    Update vtbs.js

--- api/vtbs.js
+++ api/vtbs.js
@@ -1,4 +1,15 @@
 module.exports = [
   { mid: 349991143, note: 'Mea' },
-  { mid: 345819190, note: 'Kanna' }
+  { mid: 345819190, note: 'Kanna' },
+  { mid: 389862071, note: 'Mio' },
+  { mid: 332704117, note: 'Fubuki' },
+  { mid: 375504219, note: 'Aqua' },
+  { mid: 265224956, note: 'Luna' },
+  { mid: 336731767, note: 'xia' },
+  { mid: 339567211, note: 'heart' },
+  { mid: 296909317, note: 'himehina' },
+  { mid: 3149619, note: 'mana' },
+  { mid: 286179206, note: 'Sora' },
+  { mid: 389857640, note: 'Shion' },
+  { mid: 389858027, note: 'Oni' }
 ]
