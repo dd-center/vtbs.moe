440c5fd25a46a31a9435b3b51391f6c606e0b957
simon3000 <simon3000@163.com>
2019-04-16 02:56:36 +0800

    Update vtbs.js

--- api/vtbs.js
+++ api/vtbs.js
@@ -1,41 +1,104 @@
 module.exports = [
-  { mid: 349991143, note: 'Mea' },
+  // Project Paryi
+  { mid: 1576121, note: 'Paryi' },
   { mid: 372984197, note: 'Shiori' },
-  { mid: 12362451, note: 'homolive' },
-  { mid: 1473830, note: 'KIZUNA.AI' },
-  { mid: 291296062, note: '猫宫' },
+  { mid: 349991143, note: 'Mea' },
   { mid: 8119834, note: 'Hana' },
   { mid: 2778044, note: 'Miu' },
+  { mid: 3149619, note: 'mana' },
   { mid: 6055289, note: 'YYUT(x)' },
-  { mid: 18149131, note: '🔨' },
-  { mid: 27534330, note: '爱酱' },
-  { mid: 3772162, note: '清露' },
   { mid: 380829248, note: '救救猫猫' },
   { mid: 1869304, note: 'ENA' },
+
+  // Chukolala
   { mid: 406805563, note: 'Oto' },
   { mid: 3568142, note: '果冻妹' },
-  { mid: 1576121, note: 'Paryi' },
   { mid: 407067880, note: '伞' },
   { mid: 1498668, note: 'Chisuzu' },
   { mid: 2299184, note: 'Komori' },
   { mid: 1429475, note: 'Chiaki' },
-  { mid: 364225566, note: '战斗吧歌姬' },
-  { mid: 5570898, note: '白音小雪' },
-  { mid: 1612112, note: '魔宵' },
-  { mid: 203365648, note: 'Alice' },
+
+  // Homolive
+  { mid: 12362451, note: 'homolive' },
+
+  // Hololive
+  { mid: 286700005, note: 'hololive' },
+
+  { mid: 286179206, note: 'Sora' },
+  { mid: 20813493, note: 'roboko' },
+  { mid: 366690056, note: 'Miko' },
+
+  { mid: 389856447, note: 'MEL' },
+  { mid: 389857131, note: 'Aki' },
+  { mid: 339567211, note: 'heart' },
+  { mid: 332704117, note: 'Fubuki' },
+  { mid: 336731767, note: 'xia' },
+
+  { mid: 375504219, note: 'Aqua' },
+  { mid: 389857640, note: 'Shion' },
+  { mid: 389858027, note: 'Oni' },
+  { mid: 389858754, note: 'Choko' },
+  { mid: 389859190, note: 'Subaru' },
+
+  { mid: 389862071, note: 'Mio' },
+  { mid: 412135222, note: 'Okayu' },
+  { mid: 412135619, note: 'Korone' },
+
+  // 爱酱
+  { mid: 1473830, note: 'KIZUNA.AI' },
+  { mid: 27534330, note: '爱酱' },
+
+  // ENTUM
+  { mid: 238537745, note: '未来明' },
+  { mid: 291296062, note: '猫宫' },
+  { mid: 20709396, note: '饼' },
+
+  // oveRidea
+  { mid: 18149131, note: '🔨' },
+
+  // 艾琳
   { mid: 292044559, note: 'Yomemi' },
   { mid: 355167926, note: 'Englisher' },
+
+  // sora4
+  { mid: 5570898, note: '白音小雪' },
+
+  // Winks
+  { mid: 337827661, note: '陆婉莹' },
+
+  { mid: 359082339, note: 'Pro' },
+
+  { mid: 387108043, note: '莉姬' },
+  { mid: 1826804, note: '爱夏' },
+
+  // KMNHZ
+  { mid: 2149710, note: 'KMNHZ' },
+  { mid: 397520517, note: 'LIZ' },
+  { mid: 397520048, note: 'LITA' },
+
+  // Office339
+  { mid: 267970450, note: 'DD' },
+  { mid: 382821371, note: 'JJ' },
+  { mid: 24622055, note: 'GG' },
+
+  // Unlimited
+  { mid: 252599, note: '游戏部' },
+  { mid: 381951501, note: 'Cocoa' },
+
+  // 上海2434
   { mid: 1631724, note: '塞塔nya' },
-  { mid: 238537745, note: '未来明' },
+
+  // 无所属/无细分 (懒)
+  { mid: 386900246, note: '鬼妈' },
+  { mid: 3772162, note: '清露' },
+  { mid: 364225566, note: '战斗吧歌姬' },
+  { mid: 1612112, note: '魔宵' },
+  { mid: 203365648, note: 'Alice' },
   { mid: 35749736, note: '子彧' },
   { mid: 80387576, note: 'Hime' },
-  { mid: 252599, note: '游戏部' },
   { mid: 125823832, note: '琥珀-虚颜' },
   { mid: 31761638, note: '8911勘测研究所' },
-  { mid: 337827661, note: '陆婉莹GodRiku' },
-  { mid: 24622055, note: '虚拟宫宫Official' },
   { mid: 5563350, note: '虚拟次元计划' },
-  { mid: 1826804, note: '爱夏なつ' },
   { mid: 388693909, note: 'Mendako醤Official' },
   { mid: 397723865, note: '竹轮喵Official' },
   { mid: 483231, note: '乐喵Channel' },
@@ -54,23 +117,8 @@ module.exports = [
   { mid: 403768239, note: 'RukiRokiChannel' },
   { mid: 320740464, note: '兰若_Ruo' },
   { mid: 345819190, note: 'Kanna' },
-  { mid: 389862071, note: 'Mio' },
-  { mid: 332704117, note: 'Fubuki' },
-  { mid: 375504219, note: 'Aqua' },
   { mid: 265224956, note: 'Luna' },
-  { mid: 336731767, note: 'xia' },
-  { mid: 339567211, note: 'heart' },
   { mid: 296909317, note: 'himehina' },
-  { mid: 3149619, note: 'mana' },
-  { mid: 286179206, note: 'Sora' },
-  { mid: 389857640, note: 'Shion' },
-  { mid: 389858027, note: 'Oni' },
-  { mid: 20813493, note: 'roboko' },
-  { mid: 366690056, note: 'Miko' },
-  { mid: 389856447, note: 'MEL' },
-  { mid: 389857131, note: 'Aki' },
-  { mid: 389858754, note: 'Choko' },
-  { mid: 389859190, note: 'Subaru' },
   { mid: 403927583, note: '椎名唯华' },
   { mid: 403930401, note: '绿仙' },
   { mid: 403928672, note: '葵' },
