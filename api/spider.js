89ed3a08fa6dfc31ebfe367a684de39c4b89a42c
simon3000 <simon3000@163.com>
2019-04-18 08:51:00 +0800

    auto retry

--- api/spider.js
+++ api/spider.js
@@ -57,7 +57,12 @@ class Spider {
     for (let i = this.spiderId; i < this.vtbs.length; i += this.PARALLEL) {
       let vtb = this.vtbs[i]
       let time = (new Date()).getTime()
-      let object = await biliAPI(vtb, ['mid', 'uname', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'online', 'face', 'areaRank'])
+      let object = await biliAPI(vtb, ['mid', 'uname', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'online', 'face', 'areaRank']).catch(() => undefined)
+      if (!object) {
+        i -= this.PARALLEL
+        this.log(`RETRY: ${vtb.mid}`)
+        continue
+      }
       let { mid, uname, roomid, sign, notice, follower, archiveView, guardNum, liveStatus, online, face, areaRank } = object
 
       let info = await this.db.info.get(mid)
