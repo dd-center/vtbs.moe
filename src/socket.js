70ec970245d64364d8aca5219de89d163db42149
simon3000 <simon3000@163.com>
2019-04-28 02:42:48 +0800

    use api.vtbs.moe

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
-export const socket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:8001' : 'https://api.vtb.simon3k.moe')
-// export const socket = io('https://api.vtb.simon3k.moe')
+export const socket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:8001' : 'https://api.vtbs.moe')
+// export const socket = io('https://api.vtbs.moe')
 
 export const get = async (e, target) => new Promise(resolve => {
   socket.emit(e, target, resolve)
