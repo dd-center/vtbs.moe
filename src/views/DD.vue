e523d63817548d9f71782a8974129a1dc5ea04bd
simon3000 <simon3000@163.com>
2019-05-10 07:14:49 +0800

    dd show updateTime

--- src/views/DD.vue
+++ src/views/DD.vue
@@ -8,6 +8,7 @@
       <el-col style="text-align: center;">
         <p>想要看完整列表? 可能会卡哦</p>
         <el-button type="danger" @click="loadAll" :loading="loading">看!</el-button>
+        <p v-loading="!lastUpdate">上次更新: {{lastUpdate}}</p>
       </el-col>
     </el-row>
   </el-main>
@@ -18,16 +19,20 @@
 import { get } from '@/socket'
 import d from '@/components/d'
 
+import moment from 'moment'
+
 export default {
   data: function() {
     return {
       dds: {},
       all: false,
       loading: false,
+      time: undefined,
     }
   },
   async created() {
     if (!Object.keys(this.dds).length) {
+      this.time = await get('fullGuard', 'time')
       this.dds = await get('fullGuard', 'some')
     }
   },
@@ -35,10 +40,15 @@ export default {
     rank: function() {
       let dds = { ...this.dds }
       return Object.keys(dds)
-        .filter(key => key !== 'time')
         .map(mid => dds[mid])
         .map(dd => ({ ...dd, power: dd.dd[0].length * 100 + dd.dd[1].length * 10 + dd.dd[2].length }))
         .sort((a, b) => b.power - a.power)
+    },
+    lastUpdate() {
+      if (this.time) {
+        return moment(this.time).format('M月D日 H:M')
+      }
+      return undefined
     },
   },
   methods: {
