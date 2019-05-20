484c7365f72c778a84538764ad041d9c7580f7f2
simon3000 <simon3000@163.com>
2019-05-20 14:45:40 +0800

    改改about

--- src/views/About.vue
+++ src/views/About.vue
@@ -10,12 +10,25 @@
           数据每 5 分钟更新一次<br>
           宏观经济中视频势每 6 小时更新一次<br>
           风云榜，24小时更新一次 <br>
-          名单查漏补缺 issue: <a href="https://github.com/simon300000/vtbs.moe/issues/1">https://github.com/simon300000/vtbs.moe/issues/1</a>
+          名单查漏补缺: 新建 issue <a href="https://github.com/simon300000/vtbs.moe/issues/new?assignees=simon300000&labels=&template=--vtb-vup.md&title=VTB补" target="_blank">https://github.com/simon300000/vtbs.moe/issues/new</a><br>
+          或者邮件: simon3000@163.com
           <br>
           日增的数据是过去24小时粉丝数变化，并不是昨天一天的变化
         </p>
         <a href="https://github.com/simon300000/vtbs.moe/" target="_blank"><img alt="GitHub stars" src="https://img.shields.io/github/stars/simon300000/vtbs.moe.svg?style=social"></a> <br>
         <a href="https://github.com/simon300000/vtbs.moe/" target="_blank">github:simon300000/vtbs.moe</a>
+        <br>
+        <br>
+        其他有趣的项目:
+        <br>
+        Vtuber Bilibili 直播间弹幕记录
+        <a href="https://github.com/bilibili-dd-center/bilibili-vtuber-danmaku/" target="_blank">https://github.com/bilibili-dd-center/bilibili-vtuber-danmaku/</a>
+        <br>
+        直播用 BiliChat
+        <a href="https://github.com/3Shain/BiliChat" target="_blank">https://github.com/3Shain/BiliChat</a>
+        <br>
+        BiliOB 观察者
+        <a href="https://www.biliob.com" target="_blank">https://www.biliob.com</a>
       </el-col>
       <el-col :xs="24" :span="12">
         <h1>服务器数据：</h1>
