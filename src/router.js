3b017d7dbd3529eff71045ed41443a5aa5edc437
simon3000 <simon3000@163.com>
2019-06-08 03:42:51 +0800

    About page no lazy load

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 import List from './views/List.vue'
 import DD from './views/DD.vue'
 import API from './views/API.vue'
@@ -44,7 +45,6 @@ export default new Router({
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ './views/About.vue'),
+    component: About,
   }],
 })
