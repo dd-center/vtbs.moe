cb6c166093f22427dc48956d6861fe8b0d4d0428
simon3000 <simon3000@163.com>
2019-06-10 03:50:13 +0800

    DD Infinity scroll

--- src/views/DD.vue
+++ src/views/DD.vue
@@ -2,9 +2,9 @@
 <el-container v-loading="!rank.length">
   <el-main>
     <el-row>
-      <d :dd="dd" v-for="dd in rank" :key="dd.mid"></d>
+      <d :dd="dd" v-for="dd in rankLimit" :key="dd.mid"></d>
     </el-row>
-    <el-row v-if="!all">
+    <el-row v-if="!all && allDisplay">
       <el-col style="text-align: center;">
         <p>想要看完整列表? 可能会卡哦</p>
         <el-button type="danger" @click="loadAll" :loading="loading">看!</el-button>
@@ -28,6 +28,7 @@ export default {
       all: false,
       loading: false,
       time: undefined,
+      show: 64,
     }
   },
   async created() {
@@ -35,6 +36,20 @@ export default {
       this.time = await get('fullGuard', 'time')
       this.dds = await get('fullGuard', 'some')
     }
+  },
+  mounted() {
+    this.$nextTick(function() {
+      document.onscroll = () => {
+        if (document.body.clientHeight - window.scrollY - window.innerHeight < (document.body.clientHeight / this.show * 64) && this.rank.length) {
+          if (!this.allDisplay) {
+            this.show += 32
+          }
+        }
+      }
+    })
+  },
+  destroyed() {
+    document.onscroll = null
   },
   computed: {
     rank: function() {
@@ -44,11 +59,17 @@ export default {
         .map(dd => ({ ...dd, power: dd.dd[0].length * 100 + dd.dd[1].length * 10 + dd.dd[2].length }))
         .sort((a, b) => b.power - a.power)
     },
+    rankLimit() {
+      return this.rank.filter((g, index) => index < this.show)
+    },
     lastUpdate() {
       if (this.time) {
         return moment(this.time).format('M月D日 H:M')
       }
       return undefined
+    },
+    allDisplay() {
+      return this.show >= this.rank.length
     },
   },
   methods: {
@@ -56,6 +77,7 @@ export default {
       this.loading = true
       this.dds = await get('fullGuard', 'all')
       this.all = true
+      this.show = Infinity
     },
   },
   components: {
