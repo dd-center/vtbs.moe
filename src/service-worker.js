8ee237094d080fcc3f512c2b8dbded0871d03da9
simon3000 <simon3000@163.com>
2019-06-12 15:30:06 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,4 +2,15 @@
 
 console.log('vtbs.moe 超级 ServiceWorker 参上！')
 
+workbox.routing.setDefaultHandler(
+  new workbox.strategies.StaleWhileRevalidate()
+)
+
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.setCatchHandler(({ event }) => {
+  if (event.request.destination === 'document') {
+    return caches.match('/index.html')
+  }
+  return Response.error()
+})
