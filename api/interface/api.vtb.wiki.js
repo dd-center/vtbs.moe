f0e4cc41d360fc11b2698c3c5782b6955b0756d3
simon3000 <simon3000@163.com>
2019-06-25 11:42:56 +0800

    wiki interface timeout 30s

--- api/interface/api.vtb.wiki.js
+++ api/interface/api.vtb.wiki.js
@@ -1,7 +1,7 @@
 const got = require('got')
 
 const liveHistory = async uuid => {
-  let history = (await got(`https://api.vtb.wiki/v2/bilibili/live/${uuid}/history`, { json: true }).catch(() => ({}))).body
+  let history = (await got(`https://api.vtb.wiki/v2/bilibili/live/${uuid}/history`, { json: true, timeout: 1000 * 30 }).catch(() => ({}))).body
   if (!history) {
     return undefined
   }
