cda6dd3f3421dc946446b1e65e26c5c635fb3554
simon3000 <simon3000@163.com>
2019-07-15 16:29:50 +0200

    Revert "hawk use influxDB falcon"
    This reverts commit 936528a6ec6d7c7b7ba640504df78b2ccd07245e.

--- hawk/falcon.js
+++
@@ -1,7 +0,0 @@
-const ioClient = require('socket.io-client')
-
-let falconSocket = ioClient('http://0.0.0.0:9009')
-
-const falconGet = (e, target) => new Promise(resolve => falconSocket.emit(e, target, resolve))
-
-module.exports = falconGet
--- hawk/index.js
+++ hawk/index.js
@@ -1,6 +1,6 @@
 const { join } = require('path')
 
-const falconGet = require('./falcon')
+const vdSocket = require('./vd')
 
 const Server = require('socket.io')
 const io = new Server(9011, { serveClient: false })
@@ -11,14 +11,27 @@ nodejieba.load({
   userDict: join(__dirname, 'dictionary/userdict.txt'),
 })
 
-setInterval(async () => {
-  const [danmakuHour, danmakuDay] = await Promise.all([falconGet('lastHour'), falconGet('lastDay')])
+let danmaku = []
+let danmaku1h = []
+
+vdSocket.on('danmaku', ({ message }) => {
+  danmaku.push(message)
+  danmaku1h.push(message)
+  setTimeout(() => {
+    danmaku.shift()
+  }, 1000 * 60 * 60 * 24)
+  setTimeout(() => {
+    danmaku1h.shift()
+  }, 1000 * 60 * 60)
+})
+
+setInterval(() => {
   let analyzed = {
-    h: nodejieba.extract(danmakuHour.join('\n'), 256),
-    day: nodejieba.extract(danmakuDay.join('\n'), 256),
+    day: nodejieba.extract(danmaku.join('\n'), 256),
+    h: nodejieba.extract(danmaku1h.join('\n'), 256),
   }
   io.emit('analyze', analyzed)
-  console.log(`Analyze ${danmakuHour.length}, ${danmakuDay.length}`)
+  console.log(`Analyze ${danmaku1h.length}, ${danmaku.length}`)
 }, 1000 * 60)
 
 console.log('Hawk is here')
