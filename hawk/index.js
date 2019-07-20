fb28f0e78f49fd81c884a30df135004e28845497
simon3000 <simon3000@163.com>
2019-07-20 20:43:05 +0200

    Hawk filter danmaku
    #62

--- hawk/index.js
+++ hawk/index.js
@@ -14,15 +14,45 @@ nodejieba.load({
 let danmaku = []
 let danmaku1h = []
 
-vdSocket.on('danmaku', ({ message }) => {
+const filter = message => {
+  if (message.includes('点歌')) {
+    return false
+  }
+  if (message.includes('角色')) {
+    return false
+  }
+  if (message.includes('整理')) {
+    return false
+  }
+  if (message.includes('辣条')) {
+    return false
+  }
+  if (!message.replace(/8/g, '').length) {
+    return false
+  }
+  if (!message.replace(/6/g, '').length) {
+    return false
+  }
+  if (!message.replace(/h/g, '').length) {
+    return false
+  }
+  if (message.replace(/3/g, '') === '2') {
+    return false
+  }
+  return true
+}
+
+const store = message => {
   danmaku.push(message)
   danmaku1h.push(message)
-  setTimeout(() => {
-    danmaku.shift()
-  }, 1000 * 60 * 60 * 24)
-  setTimeout(() => {
-    danmaku1h.shift()
-  }, 1000 * 60 * 60)
+  setTimeout(() => danmaku.shift(), 1000 * 60 * 60 * 24)
+  setTimeout(() => danmaku1h.shift(), 1000 * 60 * 60)
+}
+
+vdSocket.on('danmaku', ({ message }) => {
+  if (filter(message)) {
+    store(message)
+  }
 })
 
 setInterval(() => {
