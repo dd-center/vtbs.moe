f81e0fa598b0c405c43fa52b0059f78fb7742654
simon3000 <simon3000@163.com>
2019-08-10 16:26:38 +0200

    remove useless async

--- api/index.js
+++ api/index.js
@@ -19,20 +19,17 @@ const httpAPI = require('./http')
 const PARALLEL = 5
 const INTERVAL = 1000 * 60 * 5
 
-;
-(async () => {
-  const io = new Server({ serveClient: false })
-  vdb.bind(io)
-  parrot.start({ io })
-  const server = http.createServer(httpAPI({ vdb, info, fullGuard, active, live, num, macro }))
-  io.attach(server)
-  vd.attach(server)
-  spider({ PARALLEL, INTERVAL, vdb, db: { site, info, active, guard, guardType }, io, worm, parrot })
-  snake({ vdSocket, io, info })
-  hawk({ io })
-  setTimeout(() => {
-    ant({ vdb, macro, num, info, fullGuard, guardType, INTERVAL, io })
-  }, 1000 * 60 * 4)
-  io.on('connection', connect({ io, vdb, macro, site, num, info, active, guard, fullGuard, guardType, PARALLEL, INTERVAL, wormResult }))
-  server.listen(8001)
-})()
+const io = new Server({ serveClient: false })
+vdb.bind(io)
+parrot.start({ io })
+const server = http.createServer(httpAPI({ vdb, info, fullGuard, active, live, num, macro }))
+io.attach(server)
+vd.attach(server)
+spider({ PARALLEL, INTERVAL, vdb, db: { site, info, active, guard, guardType }, io, worm, parrot })
+snake({ vdSocket, io, info })
+hawk({ io })
+setTimeout(() => {
+  ant({ vdb, macro, num, info, fullGuard, guardType, INTERVAL, io })
+}, 1000 * 60 * 4)
+io.on('connection', connect({ io, vdb, macro, site, num, info, active, guard, fullGuard, guardType, PARALLEL, INTERVAL, wormResult }))
+server.listen(8001)
