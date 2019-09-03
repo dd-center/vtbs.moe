f9012592e7cbdaffd334e455e95b1d6e45cbc332
simon3000 <simon3000@163.com>
2019-09-03 02:32:26 +0200

    fix possible hawk leak

--- api/interface/hawk.js
+++ api/interface/hawk.js
@@ -1,9 +1,8 @@
 const ioClient = require('socket.io-client')
 const hawkSocket = ioClient('http://0.0.0.0:9011')
 
-let analyzed = { day: [], h: [] }
-
 module.exports = ({ io }) => {
+  let analyzed = { day: [], h: [] }
   hawkSocket.on('analyze', data => {
     analyzed = data
     io.emit('hawk', analyzed)
