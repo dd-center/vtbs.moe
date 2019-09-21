3d3ccc11e10155d8158591de6e3cde88ef73bac8
simon3000 <simon3000@163.com>
2019-09-21 23:54:18 +0200

    save live title
    close #88

--- api/snake.js
+++ api/snake.js
@@ -1,24 +1,24 @@
 module.exports = ({ vdSocket, io, info }) => {
-  let updatePending = []
+  let updatePending = new Set()
   vdSocket.on('LIVE', async ({ mid, roomid }) => {
     let currentInfo = await info.get(mid)
     currentInfo = { ...currentInfo, liveStatus: 1 }
     await info.put(mid, currentInfo)
-    updatePending.push(mid)
+    updatePending.add(mid)
     io.to(mid).emit('detailInfo', { mid, data: currentInfo })
   })
   vdSocket.on('PREPARING', async ({ mid, roomid }) => {
     let currentInfo = await info.get(mid)
     currentInfo = { ...currentInfo, liveStatus: 0, online: 0 }
     await info.put(mid, currentInfo)
-    updatePending.push(mid)
+    updatePending.add(mid)
     io.to(mid).emit('detailInfo', { mid, data: currentInfo })
   })
   vdSocket.on('ROUND', async ({ mid, roomid }) => {
     let currentInfo = await info.get(mid)
     currentInfo = { ...currentInfo, liveStatus: 0, online: 0 }
     await info.put(mid, currentInfo)
-    updatePending.push(mid)
+    updatePending.add(mid)
     io.to(mid).emit('detailInfo', { mid, data: currentInfo })
   })
   vdSocket.on('online', async ({ online, mid }) => {
@@ -27,17 +27,26 @@ module.exports = ({ vdSocket, io, info }) => {
     currentInfo = { ...currentInfo, online: liveStatus && online }
     await info.put(mid, currentInfo)
     if (online > 1) {
-      updatePending.push(mid)
+      updatePending.add(mid)
       io.to(mid).emit('detailInfo', { mid, data: currentInfo })
     }
     io.to(mid).emit('detailLive', { mid, data: { online, time: Date.now() } })
   })
+  vdSocket.on('title', async ({ mid, title }) => {
+    let currentInfo = await info.get(mid)
+    let { liveStatus } = currentInfo
+    currentInfo = { ...currentInfo, title }
+    if (liveStatus) {
+      updatePending.add(mid)
+    }
+    await info.put(mid, currentInfo)
+  })
   setInterval(async () => {
-    if (updatePending.length) {
-      io.emit('info', await Promise.all(updatePending.map(mid => info.get(mid))))
-      io.emit('log', `Snake: Refresh ${updatePending.length}`)
-      console.log(`Snake: Refresh ${updatePending.length}`)
-      updatePending = []
+    if (updatePending.size) {
+      io.emit('info', await Promise.all([...updatePending].map(mid => info.get(mid))))
+      io.emit('log', `Snake: Refresh ${updatePending.size}`)
+      console.log(`Snake: Refresh ${updatePending.size}`)
+      updatePending = new Set()
     }
   }, 1000 * 15)
 }
