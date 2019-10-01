f2768d2e790c104a85c02e05012c2e1834954346
simon3000 <simon3000@163.com>
2019-10-01 18:04:41 +0200

    non block spider core

--- api/spider.js
+++ api/spider.js
@@ -20,99 +20,107 @@ const notable = ({ object, time, currentActive }) => {
   return false
 }
 
-const round = async ({ pending, spiderId, io, db, INTERVAL, parrot, PARALLEL, biliAPI }) => {
+const core = async ({ pending, io, db, INTERVAL, parrot, biliAPI, log }) => {
+  let vtb = pending.shift()
+  if (!vtb) {
+    return []
+  }
+
+  let time = Date.now()
+
+  let object = await biliAPI(vtb, ['mid', 'uname', 'video', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'title', 'face', 'topPhoto', 'areaRank']).catch(console.error)
+  if (!object) {
+    pending.push(vtb)
+    log(`RETRY PENDING: ${vtb.mid}`)
+    return core({ pending, io, db, INTERVAL, parrot, biliAPI, log })
+  }
+
+  const rest = core({ pending, io, db, INTERVAL, parrot, biliAPI, log })
+  let { mid, uname, video, roomid, sign, notice, follower, archiveView, guardNum, liveStatus, title, face, topPhoto, areaRank, bot, uuid } = object
+
+  let averageLive = 0
+  let weekLive = 0
+
+  let liveHistory = await parrot.getLiveHistory(uuid)
+
+  let liveNum = liveHistory.LiveTime / (60 * 5)
+
+  if (liveHistory.Lives.length) {
+    averageLive = liveHistory.LiveTime * 1000 * (1000 * 60 * 60 * 24 * 7) / (time - liveHistory.Lives[0].BeginTime * 1000)
+  }
+
+  liveHistory.Lives
+    .map(({ BeginTime, EndTime }) => ({ BeginTime: BeginTime * 1000, EndTime: EndTime * 1000 }))
+    .forEach(({ BeginTime, EndTime }) => {
+      if (BeginTime > (time - 1000 * 60 * 60 * 24 * 7)) {
+        weekLive += EndTime - BeginTime
+      } else if (EndTime > (time - 1000 * 60 * 60 * 24 * 7)) {
+        weekLive += EndTime - (time - 1000 * 60 * 60 * 24 * 7)
+      }
+    })
+
+  let info = await db.info.get(mid)
+  if (!info) {
+    info = {}
+  }
+  let { recordNum = 0, guardChange = 0, online = 0 } = info
+
+  let currentActive = await db.active.get({ mid, num: recordNum })
+  if (notable({ object, time, currentActive })) {
+    recordNum++
+    io.to(mid).emit('detailActive', { mid, data: { archiveView, follower, time } })
+    await db.active.put({ mid, num: recordNum, value: { archiveView, follower, time } })
+  }
+
+  let { lastLive = {} } = info
+
+  if (liveStatus) {
+    lastLive = { online, time }
+  }
+
+  if (guardNum !== info.guardNum) {
+    guardChange++
+    io.to(mid).emit('detailGuard', { mid, data: { guardNum, time } })
+    await db.guard.put({ mid, num: guardChange, value: { guardNum, time } })
+  }
+
+  let dayNum = 1000 * 60 * 60 * 24 / INTERVAL
+  let dayBackSkip = Math.max(recordNum - dayNum, 0)
+  let totalRecordNum = Math.min(dayNum, recordNum)
+  let actives = await db.active.bulkGet({ mid, num: totalRecordNum, skip: dayBackSkip })
+  let todayActives = actives.filter(active => active.time > time - 1000 * 60 * 60 * 24)
+  let timeDifference = time - todayActives[0].time
+  let followerChange = follower - todayActives[0].follower
+  let rise = Math.round(followerChange * 1000 * 60 * 60 * 24 / timeDifference)
+
+  let guardType = await db.guardType.get(mid)
+
+  const newInfo = { mid, uuid, uname, video, roomid, sign, notice, face, rise, topPhoto, archiveView, follower, liveStatus, recordNum, guardNum, liveNum, lastLive, averageLive, weekLive, guardChange, guardType, areaRank, online, title, bot, time }
+
+  io.to(mid).emit('detailInfo', { mid, data: newInfo })
+  io.emit('spiderLeft', pending.length)
+  await db.info.put(mid, newInfo)
+
+  log(`UPDATED: ${mid} - ${uname}`)
+  return [newInfo, ...await rest]
+}
+
+const round = async ({ pending, spiderId, io, db, INTERVAL, parrot, biliAPI }) => {
   const log = log => (output => {
     console.log(output)
     io.emit('log', output)
   })(`spider ${spiderId}: ${log}`)
 
-  let infoArray = []
-  let startTime = Date.now()
+  const startTime = Date.now()
 
-  for (;;) {
-    let vtb = pending.shift()
-    let time = Date.now()
-    if (vtb) {
-      let object = await biliAPI(vtb, ['mid', 'uname', 'video', 'roomid', 'sign', 'notice', 'follower', 'archiveView', 'guardNum', 'liveStatus', 'title', 'face', 'topPhoto', 'areaRank']).catch(console.error)
-      if (!object) {
-        pending.push(vtb)
-        log(`RETRY PENDING: ${vtb.mid}`)
-        continue
-      }
-      let { mid, uname, video, roomid, sign, notice, follower, archiveView, guardNum, liveStatus, title, face, topPhoto, areaRank, bot, uuid } = object
+  const infoArray = await core({ pending, io, db, INTERVAL, parrot, biliAPI, log })
 
-      let averageLive = 0
-      let weekLive = 0
+  const time = Date.now()
 
-      let liveHistory = await parrot.getLiveHistory(uuid)
-
-      let liveNum = liveHistory.LiveTime / (60 * 5)
-
-      if (liveHistory.Lives.length) {
-        averageLive = liveHistory.LiveTime * 1000 * (1000 * 60 * 60 * 24 * 7) / (time - liveHistory.Lives[0].BeginTime * 1000)
-      }
-
-      liveHistory.Lives
-        .map(({ BeginTime, EndTime }) => ({ BeginTime: BeginTime * 1000, EndTime: EndTime * 1000 }))
-        .forEach(({ BeginTime, EndTime }) => {
-          if (BeginTime > (time - 1000 * 60 * 60 * 24 * 7)) {
-            weekLive += EndTime - BeginTime
-          } else if (EndTime > (time - 1000 * 60 * 60 * 24 * 7)) {
-            weekLive += EndTime - (time - 1000 * 60 * 60 * 24 * 7)
-          }
-        })
-
-      let info = await db.info.get(mid)
-      if (!info) {
-        info = {}
-      }
-      let { recordNum = 0, guardChange = 0, online = 0 } = info
-
-      let currentActive = await db.active.get({ mid, num: recordNum })
-      if (notable({ object, time, currentActive })) {
-        recordNum++
-        io.to(mid).emit('detailActive', { mid, data: { archiveView, follower, time } })
-        await db.active.put({ mid, num: recordNum, value: { archiveView, follower, time } })
-      }
-
-      let { lastLive = {} } = info
-
-      if (liveStatus) {
-        lastLive = { online, time }
-      }
-
-      if (guardNum !== info.guardNum) {
-        guardChange++
-        io.to(mid).emit('detailGuard', { mid, data: { guardNum, time } })
-        await db.guard.put({ mid, num: guardChange, value: { guardNum, time } })
-      }
-
-      let dayNum = 1000 * 60 * 60 * 24 / INTERVAL
-      let dayBackSkip = Math.max(recordNum - dayNum, 0)
-      let totalRecordNum = Math.min(dayNum, recordNum)
-      let actives = await db.active.bulkGet({ mid, num: totalRecordNum, skip: dayBackSkip })
-      let todayActives = actives.filter(active => active.time > time - 1000 * 60 * 60 * 24)
-      let timeDifference = time - todayActives[0].time
-      let followerChange = follower - todayActives[0].follower
-      let rise = Math.round(followerChange * 1000 * 60 * 60 * 24 / timeDifference)
-
-      let guardType = await db.guardType.get(mid)
-
-      let newInfo = { mid, uuid, uname, video, roomid, sign, notice, face, rise, topPhoto, archiveView, follower, liveStatus, recordNum, guardNum, liveNum, lastLive, averageLive, weekLive, guardChange, guardType, areaRank, online, title, bot, time }
-
-      io.to(mid).emit('detailInfo', { mid, data: newInfo })
-      io.emit('spiderLeft', pending.length)
-      await db.info.put(mid, newInfo)
-      infoArray.push(newInfo)
-
-      log(`UPDATED: ${mid} - ${uname}`)
-    } else {
-      let update = { time, spiderId: spiderId, duration: time - startTime }
-      io.emit('spiderUpdate', update)
-      await db.site.put({ mid: 'spider', num: spiderId, value: update })
-      return infoArray
-    }
-  }
+  let update = { time, spiderId: spiderId, duration: time - startTime }
+  io.emit('spiderUpdate', update)
+  await db.site.put({ mid: 'spider', num: spiderId, value: update })
+  return infoArray
 }
 
 module.exports = async ({ PARALLEL, INTERVAL, vdb, db, io, worm, parrot, biliAPI }) => {
@@ -126,7 +134,7 @@ module.exports = async ({ PARALLEL, INTERVAL, vdb, db, io, worm, parrot, biliAPI
     let startTime = Date.now()
     let pending = [...(await vdb.get())]
 
-    let spiders = Array(PARALLEL).fill().map((c, spiderId) => round({ pending, spiderId, io, db, INTERVAL, parrot, PARALLEL, biliAPI }))
+    let spiders = Array(PARALLEL).fill().map((_i, spiderId) => round({ pending, spiderId, io, db, INTERVAL, parrot, biliAPI }))
     let infoArray = [].concat(...await Promise.all(spiders))
     io.emit('info', infoArray)
 
