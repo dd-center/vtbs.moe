b7c1ad18579ac168f7a92b11c28935aa0761cc47
simon3000 <simon30002021@icloud.com>
2019-10-28 16:39:51 +0100

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
+const vue = require('@vue/cli-plugin-babel/preset')()
+
 module.exports = {
   presets: [
-    '@vue/app',
+    ...vue.presets,
   ],
   plugins: [
+    ...vue.plugins,
     '@babel/plugin-proposal-optional-chaining',
     ['@babel/plugin-proposal-pipeline-operator', { proposal: 'smart' }],
     [
