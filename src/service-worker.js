066f047ecbe2efb19e2ebdcdd4e0e78c9520136e
simon3000 <simon30002021@icloud.com>
2019-10-28 17:15:16 +0100

    cache face, no ga

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,6 +13,24 @@ workbox.routing.registerRoute(
   new workbox.strategies.NetworkOnly()
 )
 
+workbox.routing.registerRoute(
+  /https:\/\/www\.google-analytics\.com/,
+  new workbox.strategies.NetworkOnly()
+)
+
+workbox.routing.registerRoute(
+  /https:\/\/i[0123].hdslb.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'face',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 2333,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+)
+
 workbox.routing.setCatchHandler(({ event }) => {
   if (event.request.destination === 'document') {
     return caches.match('/')
