be77e2626238b714fb143fdddf656d93810e69a3
simon3000 <simon30002021@icloud.com>
2019-11-07 06:10:39 +0100

    getPending timeout

--- api/interface/state.js
+++ api/interface/state.js
@@ -1,6 +1,8 @@
 const io = require('socket.io-client')
 const socket = io('http://0.0.0.0:9200?name=vtbs.moe')
 
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 socket.on('connect', () => {
   socket.emit('join', 'all')
 })
@@ -9,6 +11,6 @@ const get = ({ name, key }) => new Promise(resolve => {
   socket.emit('state', { name, key }, resolve)
 })
 
-const getPending = () => get({ name: 'cluster', key: 'pending' })
+const getPending = () => Promise.race([get({ name: 'cluster', key: 'pending' }), wait(1000).then(() => console.error('getPending timeout'))])
 
 module.exports = { getPending, socket }
