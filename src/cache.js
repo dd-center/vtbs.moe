42c11f2098c31afe83a75ab1a9d2b5685a710147
simon3000 <simon30002021@icloud.com>
2019-11-17 18:43:12 +0100

    noCache param

--- src/cache.js
+++ src/cache.js
@@ -1,6 +1,19 @@
 import level from 'level'
 
-const cache = level('cache', { valueEncoding: 'json' })
-window.cache = cache
+const url = new URL(location.href)
 
-export default cache
+export default (() => {
+  if (url.searchParams.get('noCache')) {
+    const get = () => Promise.reject(new Error('no Cache'))
+    const put = () => undefined
+
+    return { put, get }
+  } else {
+    const cache = level('cache', { valueEncoding: 'json' })
+
+    const put = (...params) => cache.put(...params)
+    const get = (...params) => cache.get(...params)
+
+    return { put, get }
+  }
+})()
