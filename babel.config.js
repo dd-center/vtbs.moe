d2fc67a4bcb35d2a9292465290d1f669986a3e5d
simon3000 <simon30002021@icloud.com>
2020-01-13 17:14:03 +0100

    虽然不知道为什么，但是他就是修好了
    Co-Authored-By: Shigma <shigma@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,8 @@
-module.exports = api => {
-  api.cache(true)
-
-  const presets = ['@vue/cli-plugin-babel/preset']
-  const plugins = [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'smart' }],
-    ['component', { libraryName: 'element-ui', styleLibraryName: 'theme-chalk' }],
+module.exports = {
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    ['@babel/plugin-proposal-pipeline-operator', { proposal: 'smart' }]
   ]
-
-  return {
-    presets,
-    plugins,
-  }
 }
