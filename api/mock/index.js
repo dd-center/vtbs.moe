46c84a5a3eb42fbf72663ccbab8566cb5ac6ea11
simon3000 <simon30002021@icloud.com>
2020-02-23 13:11:24 +0100

    mock add relay

--- api/mock/index.js
+++ api/mock/index.js
@@ -4,3 +4,4 @@ process.env.URL = 'ws://127.0.0.1:9013'
 require('./Cluster-center/index')
 require('./DDatHome-nodejs/index')
 require('../../state-center/index')
+require('./bilibili-vtuber-live-danmaku-relay')
