c842502fdbe6829ace85f4bb0b9cbfeff571a86f
simon3000 <simon30002021@icloud.com>
2020-03-23 04:40:32 +0100

    fix mock DD@Home env

--- api/mock/index.js
+++ api/mock/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable import/first */
-// process.env.VERBOSE = 'true'
-process.env.URL = 'ws://127.0.0.1:9013'
-process.env.MOCK = true
-
 import './Cluster-center/index.js'
-import './DDatHome-nodejs/index.js'
 import '../../state-center/index.js'
 import './bilibili-vtuber-live-danmaku-relay/index.js'
 
 import '../index.js'
+
+process.env.MOCK = true
+process.env.URL = 'ws://127.0.0.1:9013'
+
+import('./DDatHome-nodejs/index.js')
