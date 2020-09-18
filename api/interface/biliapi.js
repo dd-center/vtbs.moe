cda627a0e810b2c0cc33dcc5ea47201de87d166c
simon3000 <simon30002021@icloud.com>
2020-09-18 09:44:20 +0200

    biliapi race 180ms

--- api/interface/biliapi.js
+++ api/interface/biliapi.js
@@ -3,4 +3,4 @@ import got from './cluster.js'
 
 // eslint-disable-next-line promise/param-names
 const wait = ms => new Promise((_resolve, reject) => setTimeout(reject, ms, 'timeout'))
-export const race = (object, targets, options = {}, timeout = 1000 * 60) => Promise.race([biliAPI(object, targets, { got, ...options }), wait(timeout)])
+export const race = (object, targets, options = {}, timeout = 1000 * 180) => Promise.race([biliAPI(object, targets, { got, ...options }), wait(timeout)])
