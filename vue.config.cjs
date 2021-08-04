df52a1393378b6dae8a9104ced696ec7ed5c241f
simon3000 <simon30002021@icloud.com>
2021-08-04 09:03:18 +0800

    new favicon

--- vue.config.cjs
+++ vue.config.cjs
@@ -10,10 +10,11 @@ module.exports = {
   lintOnSave: false,
   pwa: {
     iconPaths: {
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-1024x1024.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      favicon32: null,
+      favicon16: null,
+      faviconSVG: 'favicon.svg',
+      appleTouchIcon: 'favicon.svg',
+      maskIcon: 'favicon.svg',
     },
     themeColor: '#afd4e3',
     name: 'VTBs in bilibili!',
