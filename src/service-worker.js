db2a361a8cbf882ac413fdd07897b77b50e097d0
simon3000 <simon30002021@icloud.com>
2021-08-04 10:00:40 +0800

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,29 +1,32 @@
-/* eslint no-undef: 0 */
+import { precacheAndRoute } from 'workbox-precaching'
+import { registerRoute, setDefaultHandler, setCatchHandler } from 'workbox-routing'
+import { StaleWhileRevalidate, NetworkOnly, CacheFirst } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
 
 console.log('vtbs.moe 超级 ServiceWorker！')
 
-workbox.routing.setDefaultHandler(
-  new workbox.strategies.StaleWhileRevalidate()
+setDefaultHandler(
+  new StaleWhileRevalidate()
 )
 
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
+precacheAndRoute(self.__WB_MANIFEST)
 
-workbox.routing.registerRoute(
+registerRoute(
   /https:\/\/api\.vtbs\.moe/,
-  new workbox.strategies.NetworkOnly()
+  new NetworkOnly()
 )
 
-workbox.routing.registerRoute(
+registerRoute(
   /https:\/\/www\.google-analytics\.com/,
-  new workbox.strategies.NetworkOnly()
+  new NetworkOnly()
 )
 
-workbox.routing.registerRoute(
+registerRoute(
   /https:\/\/i[0123].hdslb.com/,
-  new workbox.strategies.CacheFirst({
+  new CacheFirst({
     cacheName: 'face',
     plugins: [
-      new workbox.expiration.Plugin({
+      new ExpirationPlugin({
         maxEntries: 2333,
         maxAgeSeconds: 30 * 24 * 60 * 60,
       }),
@@ -31,7 +34,7 @@ workbox.routing.registerRoute(
   })
 )
 
-workbox.routing.setCatchHandler(({ event }) => {
+setCatchHandler(({ event }) => {
   if (event.request.destination === 'document') {
     return caches.match('/')
   }
