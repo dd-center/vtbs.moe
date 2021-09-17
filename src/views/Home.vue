8d1ba7f7cfa8ff914be674b062008afd2404b5e1
simon3000 <simon30002021@icloud.com>
2021-09-17 20:40:37 +0800

    infinity scroll

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,12 @@
   <input class="input search is-rounded vtb-search" v-model="search" type="text" placeholder="查找主播~">
   <div class="columns">
     <div class="column vtb-column"></div>
-    <div class="column is-full-mobile is-11-tablet is-10-desktop is-three-fifths-widescreen is-7-fullhd">
+    <div class="column is-full-mobile is-11-tablet is-10-desktop is-three-fifths-widescreen is-7-fullhd" :style="{height: sumHeight}" ref="container">
       <p v-if="cacheAge">数据缓存于: <span class="tag is-rounded is-info smallMargin">{{cacheAge}}</span></p>
       <progress class="progress" max="100" v-if="!currentVtbs.length"></progress>
+      <card v-if="mountCard" :vtb="mountCard" hover class="aboveTop" ref="mountCard"></card>
       <transition-group name="flip-list">
-        <card v-for="vtb in rankLimit" :vtb="vtb" hover :key="vtb.mid" class="card"></card>
+        <card v-for="{vtb, h} in rankLimit" :vtb="vtb" hover :key="vtb.mid" class="card" :style="{top: h, width: unitWidth}"></card>
       </transition-group>
     </div>
     <div class="column"></div>
@@ -20,37 +21,56 @@ import { mapState, mapGetters } from 'vuex'
 import card from '@/components/card'
 import moment from 'moment'
 
+const SHOW = 24
+
+const threshold = Array(2000).fill(0).map((_, i) => 1 / 2000 * i)
+
 export default {
   name: 'home',
   data() {
     return {
       show: 32,
-      search: ''
+      search: '',
+      intersectionObserver: undefined,
+      resizeObserver: undefined,
+      unitHeight: 172,
+      unitWidth: undefined,
+      ratio: 0,
     }
   },
   components: {
     card,
   },
-  methods: {},
-  mounted() {
-    this.$nextTick(function() {
-      document.onscroll = () => {
-        if (document.body.clientHeight - window.scrollY - window.innerHeight < (document.body.clientHeight / this.show * 20) && this.vtbs.length) {
-          if (!this.allDisplay) {
-            this.show += 15
-          } else {
-            document.onscroll = null
-          }
+  watch: {
+    mountCard: {
+      immediate: true,
+      async handler() {
+        await this.$nextTick()
+        if (this.$refs.mountCard) {
+          this.resizeObserver.observe(this.$refs.mountCard.$el)
         }
-      }
-    })
-  },
-  destroyed() {
-    document.onscroll = null
+      },
+    },
   },
   computed: {
     ...mapState(['currentVtbs', 'cachedTime']),
     ...mapGetters(['vtbs', 'followerRank', 'liveRank', 'riseRank']),
+    baseHeight() {
+      if (this.cacheAge) {
+        return 57 + 86
+      }
+      return 57
+    },
+    mountCard() {
+      return this.preRank[0]
+    },
+    sumHeight() {
+      return `${this.preRank.length * this.unitHeight}px`
+    },
+    skip() {
+      const total = this.preRank.length
+      return Math.max(0, Math.min(total - SHOW, Math.floor(this.ratio * total)))
+    },
     cacheAge() {
       if (!this.currentVtbs.length && this.cachedTime) {
         return moment(this.cachedTime).fromNow()
@@ -70,31 +90,49 @@ export default {
       }
       return this.followerRank
     },
-    preRank: function(){
+    preRank() {
       // 临时变量, 存储将要展示的主播列表
-      let temRank = this.rank;
-      if(this.search){
-        temRank = temRank.filter(i=>{
-          if(this.$store.getters.info[i.mid]?.uname?.includes(this.search))
-            return true;
+      let temRank = this.rank
+      if (this.search) {
+        temRank = temRank.filter(i => {
+          if (((this.$store.getters.info[i.mid] || {}).uname || []).includes(this.search)) {
+            return true
+          }
         })
       }
       return temRank
     },
     rankLimit: function() {
       return this.preRank
-        .filter((info, index) => index < this.show)
+        .slice(this.skip, this.skip + SHOW)
+        .map((vtb, i) => ({ vtb, h: `${(i + this.skip) * this.unitHeight + this.baseHeight}px` }))
     },
-    allDisplay() {
-      return this.show >= this.vtbs.length
-    },
+  },
+  created() {
+    this.resizeObserver = new ResizeObserver(([{ contentRect: { height, width } }]) => {
+      this.unitHeight = height
+      this.unitWidth = `${width}px`
+    })
+    this.intersectionObserver = new IntersectionObserver(([{ intersectionRatio }]) => {
+      this.ratio = intersectionRatio
+    }, {
+      rootMargin: '999999px 0px -150% 0px',
+      root: null,
+      threshold,
+    })
+  },
+  mounted() {
+    this.intersectionObserver.observe(this.$refs.container)
+  },
+  destroyed() {
+    this.resizeObserver.disconnect()
+    this.intersectionObserver.disconnect()
   },
 }
 </script>
 
 <style scoped>
-
-.vtb-search{
+.vtb-search {
   right: 10px;
   bottom: 10px;
   box-shadow: inset 0 0.0625em 1em rgba(10, 10, 10, 0.05);
@@ -106,12 +144,15 @@ export default {
   backdrop-filter: blur(6px) grayscale(50%);
   background-color: rgba(255, 255, 255, 0.3);
 }
-.vtb-search:focus{
+
+.vtb-search:focus {
   box-shadow: inset 0 0.0625em 1em rgba(10, 10, 10, 0.05);
 }
-.vtb-column{
+
+.vtb-column {
   padding: 25px;
 }
+
 .smallMargin {
   margin-bottom: 6px;
 }
@@ -120,24 +161,13 @@ export default {
   transition: transform 0.5s;
 }
 
-.flip-list-enter {
-  opacity: 0;
-}
-
-.flip-list-enter-active {
-  transition: opacity 0.5s;
-}
-
-.flip-list-leave {
-  display: none;
-}
-
-.flip-list-leave-active {
-  display: none;
-}
-
 .card {
-  margin-bottom: 32px;
+  position: absolute;
+}
+
+.aboveTop {
+  z-index: -20;
+  opacity: 0;
 }
 
 .gap {
