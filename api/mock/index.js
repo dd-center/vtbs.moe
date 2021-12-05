1dc73a033e1ed1c32a2ec45335fc4155feab9ad0
simon3000 <simon30002021@icloud.com>
2021-12-05 19:43:41 +0800

    Update index.js

--- api/mock/index.js
+++ api/mock/index.js
@@ -4,6 +4,7 @@ import './bilibili-vtuber-live-danmaku-relay/index.js'
 
 import '../index.js'
 
+process.env.VERBOSE = true
 process.env.MOCK = true
 process.env.URL = 'ws://127.0.0.1:9013'
 process.env.INTERVAL = '1200'
