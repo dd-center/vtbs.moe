0614caf084fb9cd1da7da26cb17539095a70cf50
simon3000 <simon30002021@icloud.com>
2022-05-11 17:58:57 +0800

    Revert "disable info emit"
    This reverts commit 070126d4b203bcea5e251aab812b705d69b9dd9e.

--- api/socket.js
+++ api/socket.js
@@ -181,6 +181,13 @@ export const connect = ({ site, info, active, guard, vdb, fullGuard, guardType, 
     console.log('user disconnected')
   })
   socket.emit('log', `ID: ${socket.id}`)
+  const vtbs = await vdb.get()
+  socket.emit('vtbs', vtbs.filter(({ uuid }) => uuid !== '9c1b7e15-a13a-51f3-88be-bd923b746474'))
+  const infoArray = (await Promise.all(vtbs.map(({ mid }) => mid).map(mid => info.get(mid))))
+    .filter(Boolean)
+    .map(infoFilter)
+
+  socket.emit('info', infoArray)
 
   socket.emit('worm', wormResult())
 
