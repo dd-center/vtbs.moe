f36ae444e216ca443a0d671d2031a642401e4346
simon3000 <simon30002021@icloud.com>
2022-05-11 21:33:09 +0800

    Revert "Revert "infoArray no passive update""
    This reverts commit 73d816513ef63ad8ce8c324e459bb5b18dcf8d9a.

--- api/socket.js
+++ api/socket.js
@@ -5,28 +5,40 @@ import { macro, num, info } from './database.js'
 import { io } from './interface/io.js'
 import { vdb } from './interface/index.js'
 
+const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 const deflateAsync = promisify(deflate)
 
 export const infoFilter = ({ mid, uuid, uname, roomid, sign, face, rise, archiveView, follower, liveStatus, guardNum, lastLive, guardType, online, title }) => ({ mid, uuid, uname, roomid, sign, face, rise, archiveView, follower, liveStatus, guardNum, lastLive, guardType, online, title })
 
 const metaMap = new WeakMap()
 
-let infoArray = []
-let infoArrayLastUpdate = 0
-const updateInfoArray = async () => {
-  if (infoArrayLastUpdate + 1000 * 30 > Date.now()) {
+const infoArrayPending = new Set()
+let infoArrayLock = Promise.resolve()
+
+const emitInfoArray = async () => {
+  if (!infoArrayPending.size) {
     return
   }
-  infoArrayLastUpdate = Date.now() + 1000 * 30
   const vtbs = await vdb.getPure()
-  infoArray = (await Promise.all(vtbs.map(({ mid }) => mid).map(mid => info.get(mid))))
+  const infoArray = (await Promise.all(vtbs.map(({ mid }) => mid).map(mid => info.get(mid))))
     .filter(Boolean)
     .map(infoFilter)
-  infoArrayLastUpdate = Date.now()
-  console.log('updateInfoArray')
+  const pending = [...infoArrayPending]
+  infoArrayPending.clear()
+  pending.filter(socket => socket.connected)
+    .forEach(socket => {
+      socket.emit('vtbs', vtbs)
+      socket.emit('info', infoArray)
+    })
+  console.log('emitInfoArray')
+  await wait(1000 * 10)
 }
 
-await updateInfoArray()
+const sendInfoArray = socket => {
+  infoArrayPending.add(socket)
+  infoArrayLock = infoArrayLock.then(emitInfoArray).catch(console.error)
+}
 
 const wsRouter = ({ socket }) => ([key, ...rest], map = []) => {
   if (map.includes(key)) {
@@ -199,10 +211,7 @@ export const connect = ({ site, active, guard, fullGuard, guardType, PARALLEL, I
     console.log('user disconnected')
   })
   socket.emit('log', `ID: ${socket.id}`)
-  const vtbs = await vdb.getPure()
-  socket.emit('vtbs', vtbs)
-  socket.emit('info', infoArray)
-  updateInfoArray()
+  sendInfoArray(socket)
 
   socket.emit('worm', wormResult())
 
