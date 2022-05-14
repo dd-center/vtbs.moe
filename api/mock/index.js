a0d7ac5c51c2bd8ed7e69bf6d8cabc5e7cd918af
simon3000 <simon30002021@icloud.com>
2022-05-15 00:23:21 +0800

    mock delay index

--- api/mock/index.js
+++ api/mock/index.js
@@ -2,10 +2,9 @@ import './Cluster-center/index.js'
 import '../../state-center/index.js'
 import './bilibili-vtuber-live-danmaku-relay/index.js'
 
-import '../index.js'
-
 process.env.VERBOSE = true
 process.env.MOCK = true
 process.env.URL = 'ws://127.0.0.1:9013'
 process.env.INTERVAL = '1200'
+import('../index.js')
 import('./DDatHome-nodejs/index.js')
