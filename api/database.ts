92fbfd9e374e26d56bb42fbe81b258a2f7d0d289
simon3000 <simon30002021@icloud.com>
2022-06-01 17:45:48 +0200

    Revert "Revert "merge of conflict""
    This reverts commit 1063acfdb27d958bfd339a3bf501c82f830f1bcf.

--- api/database.ts
+++ api/database.ts
@@ -58,6 +58,7 @@ export const num = new LevelDatabase({ name: 'num', db })
 
 export const status = new SubLevelDatabase<string, number>({ name: 'status', db })
 export const roomidMap = new SubLevelDatabase<number, number>({ name: 'roomidMap', db })
+export const queue = new SubLevelDatabase<string, string>({name: 'queue', db})
 
 export const info = new LevelDatabase({ name: 'info', db })
 export const active = new ArrayDatabase({ name: 'active', db })
