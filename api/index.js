1063acfdb27d958bfd339a3bf501c82f830f1bcf
simon3000 <simon30002021@icloud.com>
2022-06-01 17:40:50 +0200

    Revert "merge of conflict"
    This reverts commit a6ee38d2c5a15bb84f3746135ae9bbdd07c15bfa.

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import http from 'http'
 
 import { vd, vdSocket, hawk, vdb, biliAPI, stateSocket, cState, io } from './interface/index.js'
 
-import { site, num, info, active, live, guard, macro, fullGuard, guardType, status, queue } from './database.js'
+import { site, num, info, active, live, guard, macro, fullGuard, guardType, status } from './database.js'
 
 import snake from './snake.js'
 import { worm, wormResult } from './worm.js'
@@ -22,8 +22,7 @@ vdb.bind(io)
 const server = http.createServer(httpAPI({ info, fullGuard, active, live, num, macro, guard }))
 io.attach(server)
 vd.attach(server)
-console.log("starting spider")
-spider({ INTERVAL, vdb, db: { site, info, active, guard, guardType, status, queue }, io, worm, biliAPI, infoFilter })
+spider({ INTERVAL, vdb, db: { site, info, active, guard, guardType, status }, io, worm, biliAPI, infoFilter })
 snake({ vdSocket, io, info })
 hawk({ io })
 ant({ vdb, macro, num, info, fullGuard, guardType, INTERVAL, io, biliAPI })
