a6b3de2de39a132b7dd9a469210fd8f33bdb6209
simon3000 <simon30002021@icloud.com>
2022-06-01 17:45:46 +0200

    Revert "Revert "merge of conflict""
    This reverts commit 8848935ab3931d93483cb6a39e4d0e2613739f37.

--- api/spider.js
+++ api/spider.js
@@ -142,8 +142,23 @@ export default async ({ INTERVAL, vdb, db, io, worm, biliAPI, infoFilter }) => {
   }, 1000 * 60 * 5)
   while (true) {
     const startTime = Date.now()
-    const pending = [...(await vdb.get())]
-
+    if (await db.status.get("queueCounter") === undefined) {
+      db.status.put("queueCounter", 1)
+    }
+    let queues = ["1"]
+    const queueCounter = await db.status.get("queueCounter")
+    if(queueCounter % 7 === 0)  queues.push("2")
+    if(queueCounter % 23 === 0) queues.push("3")
+    if(queueCounter === 100 ) {
+      queues.push("4")
+      db.status.put("queueCounter", 1)
+    } else {
+      db.status.put("queueCounter", queueCounter + 1)
+    }
+    const pending = (await Promise.all(queues.map(async (q) => {
+      let queue = await db.queue.get(q)
+      return JSON.parse(queue)
+    }))).flat()
     let spiderLeft = pending.length
     io.emit('spiderLeft', spiderLeft)
     db.status.put('spiderLeft', spiderLeft)
