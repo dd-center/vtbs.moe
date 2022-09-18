ffd96a87a3f964a735a73dc06deb185cc31685a6
simon3000 <simon30002021@icloud.com>
2022-09-18 18:23:26 +0200

    Revert "oh no"
    This reverts commit 3f638c87137b85d737f3de724ddb8e88829083da.

--- api/snake.js
+++ api/snake.js
@@ -8,7 +8,7 @@ const TEN_MINUTES = 10 * 60 * 1000
 const wasOnline = new Map()
 
 const online = (id, liveStatus) => {
-  wasOnline.delete(id)
+  wasOffline.delete(id)
   const now = Date.now()
   const data = { liveStatus, now }
 
