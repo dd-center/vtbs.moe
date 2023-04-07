57e825f5ccfd27fed745366434ae3ac01aae46f5
simon3000 <simon30002021@icloud.com>
2023-04-07 13:31:48 +0200

    cdn kr.vtbs.moe

--- cdn.ts
+++ cdn.ts
@@ -1,1 +1,1 @@
-export default ['https://api.vtbs.moe', 'https://cfapi.vtbs.moe', 'https://hkapi.vtbs.moe']
+export default ['https://api.vtbs.moe', 'https://cfapi.vtbs.moe', 'https://hkapi.vtbs.moe', 'https://kr.vtbs.moe']
