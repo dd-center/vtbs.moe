b8467e28882ca37db469c4d72f67fa0a1ae6e6f2
simon3000 <simon30002021@icloud.com>
2023-05-26 14:43:28 +0200

    auto salt update

--- api/interface/biliapi.ts
+++ api/interface/biliapi.ts
@@ -1,8 +1,20 @@
 import biliAPI from 'bili-api'
 import got from './cluster.js'
 
+let salt: string
+
+const updateSalt = async () => {
+  const { salt: saltResult } = await biliAPI({}, ['salt'])
+  salt = saltResult
+}
+
+await updateSalt()
+
+setTimeout(() => {
+  setInterval(updateSalt, 1000 * 60 * 60 * 1)
+}, 1000 * 60 * Math.random());
 
 // eslint-disable-next-line promise/param-names
 const wait = (ms: number) => new Promise((_resolve, reject) => setTimeout(reject, ms, 'timeout'))
-const race = (object: any, targets: string[], options = {}, timeout = 1000 * 180) => Promise.race([biliAPI(object, targets, { got, ...options }), wait(timeout)])
+const race = (object: any, targets: string[], options = {}, timeout = 1000 * 180) => Promise.race([biliAPI(object, targets, { got, salt, ...options }), wait(timeout)])
 export { race as biliAPI }
